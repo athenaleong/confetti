cf00aff629c96f116f79e2ed1cb00bc2ebc622cd
Athena Leong <athenaleong619@gmail.com>
2021-05-19 18:04:01 -0700

    added docstrings

--- src/confetti.js
+++ src/confetti.js
@@ -1,41 +1,67 @@
-export default function EmojiConfettiGenerator(params) {
+function EmojiConfettiGenerator(params) {
 
+    /**
+     * Default values
+     */
     var defaults = {
         target: 'confetti-holder', //id of the canvas 
-        max: 50,
-        animate: true,
-        emojis: ['🍍', '💛'],
-        width: window.innerWidth,
-        height: window.innerHeight,
-        startVelocity: 3,
-        rotate: true,
+        max: 50, //maximum number of particles produced
+        emojis: ['🍍', '💛'], //emojis
+        width: window.innerWidth, //dynamics height
+        height: window.innerHeight, //dynamic width
+        startVelocity: 3, //starting velocity
+        rotate: true,  //if emoji rotates
         gravity: 5,
-        sizeLower: 10,
-        sizeHigher: 90,
-        angle: 90/ 180 * Math.PI,
-        origin: {x: [500, 500], 
-                y: [300, 300]},
-        spread: 0.5 * Math.PI
+        sizeLower: 10, //lower bound of emoji size
+        sizeHigher: 90, //upper bound of emoji size
+        angle: 90/ 180 * Math.PI, //angle of explosion
+        origin: {x: [500, 505],  
+                y: [300, 305]}, //range of origin of explosion
+        spread: 0.5 * Math.PI, //spread of explosion
     };
 
+    /**
+     * Update default values according to params
+     */
+    if(params)
+        if (params.target != undefined && checkParam(params.target, 'string')) 
+            defaults.target = params.target
+        if (params.max != undefined && checkParam(params.max, 'number')) 
+            defaults.max = params.max
+        if (params.startVelocity != undefined && checkParam(params.startVelocity, 'number')) 
+            defaults.startVelocity = params.startVelocity
+        if (params.gravity != undefined && checkParam(params.gravity, 'number')) 
+            defaults.gravity = params.gravity
+        if (params.sizeLower != undefined && checkParam(params.sizeLower, 'number')) 
+            defaults.sizeLower = params.sizeHigher
+        if (params.sizeHigher != undefined && checkParam(params.sizeHigher, 'number')) 
+            defaults.sizeHigher = params.sizeHigher
+        if (params.spread != undefined && checkParam(params.spread, 'number')) 
+            defaults.spread = params.spread
+        if (params.angle != undefined && checkParam(params.angle, 'number')) 
+            defaults.angle = params.angle
+        if (params.emojis != undefined && checkEmojis(params.emojis))
+            defaults.emojis = params.emojis
+        
+        
 
     var cv = typeof defaults.target == 'object'
     ? defaults.target
     : document.getElementById(defaults.target);
     var ctx = cv.getContext("2d");
-    var particles = []; 1.8
+    var particles = []; 
 
     function rand(min, max, floor=false) {
-        if (min==max) 
-            return min
+        if (min==max) return min
         var random = Math.random() * (max - min) + min;
         return floor? Math.floor(random) : random
     }
 
-
-
+    /**
+     * Create new particle
+     */
     function emojiFactory() {
-        var emoji = defaults.emojis[rand(0, defaults.props.length, true)] 
+        var emoji = defaults.emojis[rand(0, defaults.emojis.length, true)] 
         var velocity = rand(defaults.startVelocity * 0.7, defaults.startVelocity * 3)
         var angle = rand(defaults.angle - defaults.spread, defaults.angle + defaults.spread)
         var velocityY = (angle % 360 > Math.PI) ? velocity * Math.sin(2 * Math.PI - angle) : velocity * - Math.sin(angle) 
@@ -55,37 +81,25 @@ export default function EmojiConfettiGenerator(params) {
         return p
     }
 
-    function emojiDraw(emoji) {
-        if (emoji){
-            ctx.save();
-            ctx.font = emoji.size + "px Arial";
-            ctx.textAlign = "center";
-            ctx.textBaseline= "middle";
-            if (defaults.rotate) {
-                ctx.translate(emoji.x, emoji.y);
-                ctx.rotate(emoji.rotate);
-                ctx.translate(-1 * emoji.x, -1 * emoji.y);
-            }
-            ctx.fillText(emoji.emo, emoji.x, emoji.y);
-            ctx.restore();
-        }
-    }
 
+    /**
+     * Update particle location, angel of rotation and velocity
+     * @returns 
+     */
     function update() {
         for (var i in particles){
             var emoji = particles[i]
             if (emoji) {
-
-                //Update x, y location
-                var angle = emoji.angle - Math.PI; 
+                //Update x, y location 
                 emoji.y = emoji.y + emoji.velocityY+ 0.5 * defaults.gravity;
                 emoji.x = emoji.x + emoji.velocityX;
 
                 // Update velocity and rotation
                 emoji.velocityY += defaults.gravity/100;
-                emoji.rotate += emoji.rotateSpeed;
+                if (defaults.rotate)
+                    emoji.rotate += emoji.rotateSpeed;
 
-                // Update emoji if 
+                // Set particle as undefined if leaves screen
                 if(emoji.y > defaults.height) {
                     particles[i] = undefined;
                 }
@@ -95,41 +109,60 @@ export default function EmojiConfettiGenerator(params) {
         if (particles.every(function(p) { return p === undefined; })) {
             _clear();
         }
-
-        return requestAnimationFrame(draw);
+        else {
+            return requestAnimationFrame(draw);
+        }
     }
 
+    /** 
+     * Add particle to canvas
+     */
     function draw(){
         ctx.clearRect(0, 0, defaults.width, defaults.height);
 
-        for(var i in particles)
-          emojiDraw(particles[i]);
+        for(var i in particles) {
+            var emoji = particles[i]
+            if (emoji){
+                ctx.save();
+                ctx.font = emoji.size + "px Arial";
+                ctx.textAlign = "center";
+                ctx.textBaseline= "middle";
+                if (defaults.rotate) {
+                    ctx.translate(emoji.x, emoji.y);
+                    ctx.rotate(emoji.rotate);
+                    ctx.translate(-1 * emoji.x, -1 * emoji.y);
+                }
+                ctx.fillText(emoji.emo, emoji.x, emoji.y);
+                ctx.restore();
+            }
+        }
 
-        update(particles);
+        update();
 
     }
 
+    /**
+     * Render Canvas
+     */
     var _render = function() {
         cv.width = defaults.width;
         cv.height = defaults.height;
         for (var i=0; i < defaults.max; i++) {
             particles.push(emojiFactory())
         }
-        for(var i in particles) {
-            emojiDraw(particles[i])
-        }
-        update()
-
-        requestAnimationFrame(draw)
+        
+        draw()
     }
 
+    /**
+     * Clear Canvas
+     */
     var _clear = function() {
-    defaults.animate = false;
     requestAnimationFrame(function() {
         ctx.clearRect(0, 0, cv.width, cv.height);
-      var w = cv.width;
-      cv.width = 1;
-      cv.width = w;
+    //   var w = cv.width;
+    //   cv.width = 1;
+    //   cv.width = w;
     });
   }
     
@@ -139,3 +172,17 @@ export default function EmojiConfettiGenerator(params) {
     }
 
 }
+
+function checkParam(variable, type) {
+    if (typeof variable !== type) throw new Error(`${variable} is not a ${type}`)
+    return true
+}
+
+function checkEmojis(variable) {
+    if (!Array.isArray(variable)) throw new Error("Array needed for Emojis")
+    if (!variable.every((e) => {return typeof e === "string"})) throw new Error("Strings needed for emoji array")
+
+    return true 
+}
+
+export default EmojiConfettiGenerator
